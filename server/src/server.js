9475e15f22acbf5bed6e1c54cbe066788a86846e
Divyansh Raj Tripathi <drt.vip777@gmail.com>
2026-01-28 23:47:49 +0530

    Add production-ready MongoDB connection setup

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,17 @@
 import dotenv from "dotenv";
+dotenv.config(); // ✅ sabse pehle
+console.log("MONGO ENV:", process.env.MONGODB_URI); // 
 import app from "./app.js";
-
-dotenv.config();
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,() => {
-   console.log(`Server is Running on PORT ${PORT}`);
-});
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT} 🚀`);
+  });
+};
+
+startServer();
